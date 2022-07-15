bd32b390b2cc8128dd09bd6aaec89d319942b8de
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2022-07-15 17:55:53 -0300

    tentando normalizar pagination

--- src/app/home/recent-activities/recent-activities.component.ts
+++ src/app/home/recent-activities/recent-activities.component.ts
@@ -10,6 +10,8 @@ export class RecentActivitiesComponent implements OnInit {
 
   public activities: any;
   page = 1;
+  pageSize = 4;
+  collectionSize:any;
 
   constructor(private githubService: GithubService) { }
 
@@ -28,11 +30,26 @@ export class RecentActivitiesComponent implements OnInit {
           }
         }
 
-        this.activities = pushOnlyList
+        this.activities = pushOnlyList.map(e => {
+
+          return {...e, count: 1+1};
+        });
+        this.collectionSize = pushOnlyList.length;
+
         console.log(this.activities);
       },
       error: e => console.log
     })
+  }
+
+  refreshCountries() {
+    this.activities = this.activities
+      .map((country:any, i:any) => {
+        console.log({id: i + 1, ...country});
+
+        ({id: i + 1, ...country})
+      })
+      .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
   }
 
 }
